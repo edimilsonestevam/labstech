45fe150d9e4bed079e5865a79e6b2d187fb64482
Edimilson Estevam <edimilson.cruz-terceiro@cea.com.br>
2020-11-08 20:13:32 +0000

    Technical Challenges

---
+++ petz/petz-api/src/test/java/pt/po/edimilsonestevam/stepDefinition/TestSteps.java
@@ -0,0 +1,17 @@
+package pt.po.edimilsonestevam.stepDefinition;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Entao;
+import cucumber.api.java.pt.Quando;
+
+
+public class TestSteps {
+	
+	protected static String url = "";
+	
+
+}
