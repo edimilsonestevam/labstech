ce61b824a5cc74614352c6682a2a7b3aebedff8b
Edimilson Estevam <edimilsonestevam@gmail.com>
2020-11-12 01:40:39 +0000

    Technical Challenges

--- petz/petz-api/src/test/java/pt/po/edimilsonestevam/stepDefinition/TestSteps.java
+++ petz/petz-api/src/test/java/pt/po/edimilsonestevam/stepDefinition/TestSteps.java
@@ -10,8 +10,6 @@ import junitparams.mappers.CsvWithHeaderMapper;
 import pt.po.edimilsonestevam.service.MapaCidade;
 
 public class TestSteps {
-	
-	protected String uri = "http://api.openweathermap.org/data/2.5/weather?q=porto&appid=1657f46b179509c93448fae18c78680d";
 			
 	MapaCidade usuario = new MapaCidade();
 	
@@ -27,7 +25,7 @@ public class TestSteps {
 	@Test
 	@FileParameters(value = "src/test/java/resources/MassaDeDados.csv", mapper = CsvWithHeaderMapper.class)
 	@Quando("^eu informo o nome da cidade para consulta \"([^\"]*)\"$")
-	public void eu_informo_o_nome_da_cidade_para_consulta(String cidade) throws Throwable {
+	public void eu_informo_o_nome_da_cidade_para_consulta(String uri, String cidade) throws Throwable {
 
 		usuario.validarCidade(uri, cidade);
 		
@@ -36,7 +34,7 @@ public class TestSteps {
 	@Test
 	@FileParameters(value = "src/test/java/resources/MassaDeDados.csv", mapper = CsvWithHeaderMapper.class)
 	@Entao("^o serviço apresenta o codigo do pais (\\d+), sigla do pais \"([^\"]*)\"$")
-	public void o_serviço_apresenta_o_codigo_do_pais_sigla_do_pais(int codigoPais, String siglaPais) throws Throwable {
+	public void o_serviço_apresenta_o_codigo_do_pais_sigla_do_pais(String uri, int codigoPais, String siglaPais) throws Throwable {
 		
 		usuario.validarCodigoPais(uri, codigoPais);
 		usuario.validarSiglaPais(uri, siglaPais);
