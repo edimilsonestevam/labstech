f2973a7ffdbf869b8d1e36bee244de391facc79e
Edimilson Estevam <edimilsonestevam@gmail.com>
2021-11-30 02:15:23 +0000

    Technical Challenges
    I am adding web testing automation in the repository.

---
+++ letsgetchecked/letsgetchecked/src/test/java/pt/po/edimilsonestevam/page/ConsentGoogle.java
@@ -0,0 +1,29 @@
+package pt.po.edimilsonestevam.page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import pt.po.edimilsonestevam.setup.Base;
+
+public class ConsentGoogle extends Base{
+
+	public ConsentGoogle(WebDriver browser) {
+		super(browser);
+	
+	}
+	
+	public void ConsentGoogleConfirm () {
+		
+		String btnAccept = "//span[contains(text(),'Aceito')]";
+		
+		WebElement element = null;
+
+		WebDriverWait wait = new WebDriverWait(browser, 5);
+		element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(btnAccept)));
+		element.click();
+		
+	}
+}
---
+++ letsgetchecked/letsgetchecked/src/test/java/pt/po/edimilsonestevam/setup/Base.java
@@ -0,0 +1,15 @@
+package pt.po.edimilsonestevam.setup;
+
+import org.openqa.selenium.WebDriver;
+
+public class Base {
+
+	protected WebDriver browser;
+	
+	public Base(WebDriver browser) {
+		
+		this.browser = browser;
+		
+	}
+	
+}
