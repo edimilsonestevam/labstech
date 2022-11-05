641eee48c7aa718c0b5c716addd7beddaac8f1fd
Edimilson Cruz <edimilson.cruz@taking.com.br>
2022-11-05 18:32:37 +0000

    Technical Challenges
    I am adding Cart class in the repository.

---
+++ tamanna/tamanna/src/test/java/pt/po/edimilsonestevam/page/Cart.java
@@ -0,0 +1,38 @@
+package pt.po.edimilsonestevam.page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import junit.framework.Assert;
+import pt.po.edimilsonestevam.setup.Base;
+
+public class Cart extends Base {
+
+	public Cart(WebDriver browser) {
+		super(browser);
+
+	}
+
+	public void removeItemsCart() {
+
+		System.out.println("• Removing Items Cart...");
+
+		String removeButton = "//body/div[@id='__next']/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/button[1]/span[1]/*[1]";
+
+		browser.findElement(By.xpath(removeButton)).click();
+
+	}
+	
+	public void checkItemsPrice () {
+		
+		System.out.println("• Checking Items Price...");
+		
+		String price = "total-value";	
+		String currentPriceValue = browser.findElement(By.id(price)).getText();
+		String expectedPriceValue = "KWD 0.000";
+		
+		Assert.assertEquals("Checking Items Price", expectedPriceValue, currentPriceValue);
+		
+	}
+	
+}
