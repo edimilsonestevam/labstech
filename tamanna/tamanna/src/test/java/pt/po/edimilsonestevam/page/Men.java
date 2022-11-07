8023ce17de8c1857dc97c62a9cbbf4e9d0f42215
Edimilson Cruz <edimilson.cruz@taking.com.br>
2022-11-07 14:55:41 +0000

    Technical Challenges
    I am removing the chooseClothingCategory and chooseClothing methods from the Men class.

--- tamanna/tamanna/src/test/java/pt/po/edimilsonestevam/page/Men.java
+++ tamanna/tamanna/src/test/java/pt/po/edimilsonestevam/page/Men.java
@@ -12,26 +12,6 @@ public class Men extends Base {
 		super(browser);
 
 	}
-
-	public void chooseClothingCategory() {
-		
-		System.out.println("• Choosing Clothing Category...");
-		
-		String clothing = "//a[contains(text(),'Clothing')]";
-		
-		browser.findElement(By.xpath(clothing));
-		
-	}
-	
-	public void chooseClothing() {
-		
-		System.out.println("• Choosing Clothing...");
-		
-		String clothing = "//body/div[@id='__next']/div[1]/div[2]/main[1]/div[1]/div[3]/div[2]/div[2]/div[2]/div[2]/div[1]/a[1]/div[1]/span[1]/img[1]";
-		
-		browser.findElement(By.xpath(clothing)).click();
-		
-	}
 	
 	public void chooseSubcategory(String subcategory) {
 		
