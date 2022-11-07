1d9553e3449a0795504b04f48caa0b1206852583
Edimilson Cruz <edimilson.cruz@taking.com.br>
2022-11-07 23:02:32 +0000

    Technical Challenges
    I am adding the chooseMenClothing method in the Men class

--- tamanna/tamanna/src/test/java/pt/po/edimilsonestevam/page/Men.java
+++ tamanna/tamanna/src/test/java/pt/po/edimilsonestevam/page/Men.java
@@ -63,4 +63,61 @@ public class Men extends Base {
 		}
 		
 	}
+	
+	public void chooseMenClothing(String imageNumber) {
+		
+		System.out.println("• Choosing Clothing...");
+		
+		String imageMenFirst = "1";
+		String imageMenSecond = "2";
+		String imageMenThird = "3";
+		String imageMenFour = "4";
+		String imageMenFifth = "5";
+		String imageMenSixth = "6";
+		String imageMenSeventh = "7";
+		
+		String imageMenFirstPath = "//img[@alt='AE 10\" Classic Board Short']";
+		String imageMenSecondPath = "//img[@alt='AE Super Soft Icon V-Neck T-Shirt']";
+		String imageMenThirdPath = "//img[@alt='Achilles Organic Cotton Oversized Unisex Sweatshirt']";
+		String imageMenFourPath = "//img[@alt='Anthony Bamboo Viscose Oversized Short Sleeve T-Shirt With Arabic Printed Hem']";
+		String imageMenFifthPath = "//img[@alt='Aryan Organic Cotton Oversized Unisex Short Sleeve Hoodie']";
+		String imageMenSixthPath = "//img[@alt='Ace Recycled Crinkle Nylon Reversible Bomber Jacket']";
+		String imageMenSeventhPath = "//img[@alt='Fleece Jogger']";
+		
+		WebElement image = null;
+		WebDriverWait wait = new WebDriverWait(browser, 10);
+		
+		if (imageNumber.equalsIgnoreCase(imageMenFirst)) {
+			image = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(imageMenFirstPath)));
+			image.click();
+		}
+		else if (imageNumber.equalsIgnoreCase(imageMenSecond)) {	
+			image = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(imageMenSecondPath)));
+			image.click();
+		}
+		else if (imageNumber.equalsIgnoreCase(imageMenThird)) {
+			image = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(imageMenThirdPath)));
+			image.click();
+		}
+		else if (imageNumber.equalsIgnoreCase(imageMenFour)) {
+			image = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(imageMenFourPath)));
+			image.click();
+		}
+		else if (imageNumber.equalsIgnoreCase(imageMenFifth)) {
+			image = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(imageMenFifthPath)));
+			image.click();
+		}
+		else if (imageNumber.equalsIgnoreCase(imageMenSixth)) {
+			image = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(imageMenSixthPath)));
+			image.click();
+		}
+		else if (imageNumber.equalsIgnoreCase(imageMenSeventh)) {
+			image = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(imageMenSeventhPath)));
+			image.click();
+		}
+		else {
+			System.out.println("You should inform: 1, 2, 3, 4, 5, 6 or 7");
+		}
+		
+	}
 }
