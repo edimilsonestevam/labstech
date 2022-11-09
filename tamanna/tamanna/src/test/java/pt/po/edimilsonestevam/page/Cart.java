0923ac390edaf5178cc0344a737a6d031caf1655
Edimilson Cruz <edimilson.cruz@taking.com.br>
2022-11-09 15:03:19 +0000

    Technical Challenges
    I refactored the Cart class.

--- tamanna/tamanna/src/test/java/pt/po/edimilsonestevam/page/Cart.java
+++ tamanna/tamanna/src/test/java/pt/po/edimilsonestevam/page/Cart.java
@@ -42,45 +42,55 @@ public class Cart extends Base {
 		Assert.assertEquals("Checking Items Price", expectedPriceValue, currentPriceValue);
 
 	}
-	
+
 	public void checkItemsTotal(String expectedItemsTotal) {
 
 		System.out.println("• Checking Items Total...");
 
-		String itemsTotal = "//h3[contains(text(),'" + expectedItemsTotal + " item')]";
+		String itemsTotal = "//h3[contains(text(),'" + expectedItemsTotal + " items')]";
 		String currentItemsTotal = browser.findElement(By.xpath(itemsTotal)).getText();
 
 		Assert.assertEquals("Checking Items Total", expectedItemsTotal, currentItemsTotal);
 
 	}
 
-	public void checkTotalPrice(String expectedTotalPrice) throws InterruptedException {
-		
+	public void checkTotalPrice(String expectedTotalPrice) {
+
 		System.out.println("• Checking Total Price...");
-		
+
 		String totalPricePath = "//body/div[@id='__next']/div[1]/div[2]/div[1]/div[1]/div[2]/div[3]/div[1]/div[3]/p[2]";
-		
+
 		WebElement totalPrice = null;
 		WebDriverWait wait = new WebDriverWait(browser, 10);
-		
+
 		totalPrice = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(totalPricePath)));
 		String currentTotalPrice = totalPrice.getText();
 
 		Assert.assertEquals("Checking Total Price", expectedTotalPrice, currentTotalPrice);
-		
+
 	}
-	
-	public void goBackHome(String expectedUrl) throws InterruptedException {
-		
+
+	public void goBackHome(String expectedUrl) {
+
 		System.out.println("• Going Home...");
-		
+
 		String homePage = "Home";
-		
+
 		browser.findElement(By.linkText(homePage)).click();
-		Thread.sleep(5);
-		String currentUrl = browser.getCurrentUrl();
-		
-		Assert.assertEquals("Checking Current URL", expectedUrl, currentUrl);
-		
+
+		try {
+
+			Thread.sleep(5);
+			String currentUrl = browser.getCurrentUrl();
+
+			Assert.assertEquals("Checking Current URL", expectedUrl, currentUrl);
+
+		} catch (InterruptedException e) {
+
+			e.printStackTrace();
+
+		}
+
 	}
+
 }
