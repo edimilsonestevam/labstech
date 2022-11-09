4000d4f143ba1f2d2938b6020168ce897c7eb510
Edimilson Cruz <edimilson.cruz@taking.com.br>
2022-11-09 06:14:05 +0000

    Technical Challenges
    I am adding the goBackHome method in the Cart class.

--- tamanna/tamanna/src/test/java/pt/po/edimilsonestevam/page/Cart.java
+++ tamanna/tamanna/src/test/java/pt/po/edimilsonestevam/page/Cart.java
@@ -2,6 +2,9 @@ package pt.po.edimilsonestevam.page;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import junit.framework.Assert;
 import pt.po.edimilsonestevam.setup.Base;
@@ -19,7 +22,12 @@ public class Cart extends Base {
 
 		String removeButton = "//body/div[@id='__next']/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/button[1]/span[1]/*[1]";
 
-		browser.findElement(By.xpath(removeButton)).click();
+		WebElement removeItems = null;
+
+		WebDriverWait wait = new WebDriverWait(browser, 10);
+
+		removeItems = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(removeButton)));
+		removeItems.click();
 
 	}
 
@@ -39,21 +47,40 @@ public class Cart extends Base {
 
 		System.out.println("• Checking Items Total...");
 
-		String itemsTotal = "//h3[contains(text(),'" + expectedItemsTotal + " items')]";
+		String itemsTotal = "//h3[contains(text(),'" + expectedItemsTotal + " item')]";
 		String currentItemsTotal = browser.findElement(By.xpath(itemsTotal)).getText();
 
 		Assert.assertEquals("Checking Items Total", expectedItemsTotal, currentItemsTotal);
 
 	}
 
-	public void checkTotalPrice(String expectedTotalPrice) {
+	public void checkTotalPrice(String expectedTotalPrice) throws InterruptedException {
 		
 		System.out.println("• Checking Total Price...");
 		
-		String totalPrice = "//body/div[@id='__next']/div[1]/div[2]/div[1]/div[1]/div[2]/div[3]/div[1]/div[3]/p[2]";
-		String currentTotalPrice = browser.findElement(By.xpath(totalPrice)).getText();
+		String totalPricePath = "//body/div[@id='__next']/div[1]/div[2]/div[1]/div[1]/div[2]/div[3]/div[1]/div[3]/p[2]";
+		
+		WebElement totalPrice = null;
+		WebDriverWait wait = new WebDriverWait(browser, 10);
+		
+		totalPrice = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(totalPricePath)));
+		String currentTotalPrice = totalPrice.getText();
 
 		Assert.assertEquals("Checking Total Price", expectedTotalPrice, currentTotalPrice);
+		
+	}
+	
+	public void goBackHome(String expectedUrl) throws InterruptedException {
+		
+		System.out.println("• Going Home...");
+		
+		String homePage = "Home";
+		
+		browser.findElement(By.linkText(homePage)).click();
+		Thread.sleep(5);
+		String currentUrl = browser.getCurrentUrl();
+		
+		Assert.assertEquals("Checking Current URL", expectedUrl, currentUrl);
 		
 	}
 }
