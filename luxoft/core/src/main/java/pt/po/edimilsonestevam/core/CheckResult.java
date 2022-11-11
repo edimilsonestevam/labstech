ecdf4f998fdd2de7e1add05b1b64b9ad122a135a
Edimilson Cruz <edimilson.cruz@taking.com.br>
2022-11-11 12:52:40 +0000

    Technical Challenges
    I am adding logic project in the repository.

---
+++ luxoft/core/src/main/java/pt/po/edimilsonestevam/core/CheckResult.java
@@ -0,0 +1,10 @@
+package pt.po.edimilsonestevam.core;
+
+public class CheckResult {
+	public static void main(String[] args) {
+		
+		NumberDivision user = new NumberDivision();
+		user.numberDivisionThree();
+		
+	}
+}
---
+++ luxoft/core/src/main/java/pt/po/edimilsonestevam/core/NumberDivision.java
@@ -0,0 +1,36 @@
+package pt.po.edimilsonestevam.core;
+
+import java.util.ArrayList;
+
+public class NumberDivision {
+
+	public void numberDivisionThree() {
+
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+		numbers.add(4);
+		numbers.add(5);
+		numbers.add(10);
+		numbers.add(15);
+
+		for (int cont = 0; cont < numbers.size(); cont++) {
+			
+			if (numbers.isEmpty()) {
+				
+				String errorResult = "Number is not divided by 3, try again!";
+				System.out.println(errorResult + numbers.get(cont));
+				
+			} else {
+				
+				if (numbers.get(cont) % 3 == 0) {
+					System.out.println(numbers.get(cont));
+					
+				}
+
+			}
+		}
+
+	}
+}
