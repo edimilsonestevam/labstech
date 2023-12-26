4453747389bd8c664f870b499e84fcfa75de5065
Edimilson Estevam <edimilsonestevam@gmail.com>
2023-12-26 18:01:58 +0000

    Technical Challenges
    I am adding Login Page in the project.

---
+++ implico/implico/src/test/java/pt/po/edimilsonestevam/page/Login.java
@@ -0,0 +1,86 @@
+package pt.po.edimilsonestevam.page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import pt.po.edimilsonestevam.setup.Base;
+
+public class Login extends Base {
+
+	public Login(WebDriver browser) {
+		super(browser);
+
+	}
+
+	public void DoLogin (String username, String password) {
+		
+		System.out.println("• Doing Login...");
+		
+		String idUsername = "user-name";
+		String idLoginPassword = "password";
+		String idLoginButton = "login-button";
+		String txtStandardUser = "standard_user";
+		String txtLockedOutUser = "locked_out_user";
+		String txtProblemUser = "problem_user";
+		String txtPerformanceGlitchUser = "performance_glitch_user";
+		String txtErrorUser = "error_user";
+		String txtVisualUser = "visual_user";
+		String txtLoginPassword = "secret_sauce";
+		
+		
+		if (username.isEmpty()) {
+			
+			System.out.println("You should inform your Username!");
+			
+		}
+		else if(username.equalsIgnoreCase(txtStandardUser)){
+			
+			browser.findElement(By.id(idUsername)).sendKeys(username);
+
+		}
+		else if(username.equalsIgnoreCase(txtLockedOutUser)) {
+			
+			browser.findElement(By.id(idUsername)).sendKeys(username);
+			
+		}
+		else if(username.equalsIgnoreCase(txtProblemUser)) {
+			
+			browser.findElement(By.id(idUsername)).sendKeys(username);
+			
+		}
+		else if(username.equalsIgnoreCase(txtPerformanceGlitchUser)) {
+			
+			browser.findElement(By.id(idUsername)).sendKeys(username);
+			
+		}
+		else if(username.equalsIgnoreCase(txtErrorUser)) {
+			
+			browser.findElement(By.id(idUsername)).sendKeys(username);
+			
+		}
+		else if(username.equalsIgnoreCase(txtVisualUser)) {
+			
+			browser.findElement(By.id(idUsername)).sendKeys(username);
+			
+		}
+		else {
+			System.out.println("You did not inform your Username corret!");
+		}
+		
+		
+		if (password.isEmpty()) {
+			
+			System.out.println("You should inform your Password!");
+			
+		}
+		else if(password.equalsIgnoreCase(txtLoginPassword)){
+			
+			browser.findElement(By.id(idLoginPassword)).sendKeys(password);
+			browser.findElement(By.id(idLoginButton)).click();
+
+		}
+		else {
+			System.out.println("You did not inform your Password corret!");
+		}
+	}
+}
