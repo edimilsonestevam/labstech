df043b0feb3b134fac9485c0018eb5cc3ba6ce2a
Edimilson Estevam <edimilsonestevam@gmail.com>
2023-12-26 17:13:42 +0000

    Technical Challenges
    I am adding TestSuite, Base, Configuration and TestSteps in the project.

---
+++ implico/implico/src/test/java/pt/po/edimilsonestevam/stepDefinition/TestSteps.java
@@ -0,0 +1,15 @@
+package pt.po.edimilsonestevam.stepDefinition;
+
+import org.openqa.selenium.WebDriver;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import pt.po.edimilsonestevam.setup.Configuration;
+
+public class TestSteps {
+
+	protected static WebDriver browser;
+	
+
+}
