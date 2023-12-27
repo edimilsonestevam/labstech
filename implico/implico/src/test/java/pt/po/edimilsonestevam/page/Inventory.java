9f0e94a9c93ef61dfdbc8d24f8735fb2b27ef14e
Edimilson Estevam <edimilsonestevam@gmail.com>
2023-12-27 00:25:02 +0000

    Technical Challenges
    I added Inventory 's addProductCart method in the project.

--- implico/implico/src/test/java/pt/po/edimilsonestevam/page/Inventory.java
+++ implico/implico/src/test/java/pt/po/edimilsonestevam/page/Inventory.java
@@ -1,5 +1,6 @@
 package pt.po.edimilsonestevam.page;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import pt.po.edimilsonestevam.setup.Base;
@@ -11,9 +12,47 @@ public class Inventory extends Base {
 	
 	}
 
-	public void addProductCart () {
+	public void addProductCart (int informProductNumber) {
 		
 		System.out.println("• Adding Product Cart...");
+		
+		String idAddProductFirst = "add-to-cart-sauce-labs-backpack";
+		String idAddProductSecond = "add-to-cart-sauce-labs-bike-light";
+		String idAddProductThird = "add-to-cart-sauce-labs-bolt-t-shirt";
+		String idAddProductFourth = "add-to-cart-sauce-labs-fleece-jacket";
+		String idAddProductFifth = "add-to-cart-sauce-labs-onesie";
+		String idAddPoductSixth = "add-to-cart-test.allthethings()-t-shirt-(red)";
+		
+		
+		switch (informProductNumber) {
+		case 1:
+			browser.findElement(By.id(idAddProductFirst)).click();
+			
+			break;
+		
+		case 2:
+			browser.findElement(By.id(idAddProductSecond)).click();
+			break;
+		
+		case 3:
+			browser.findElement(By.id(idAddProductThird)).click();
+			break;
+		
+		case 4:
+			browser.findElement(By.id(idAddProductFourth)).click();
+			break;
+		
+		case 5:
+			browser.findElement(By.id(idAddProductFifth)).click();
+			break;
+			
+		case 6:
+			browser.findElement(By.id(idAddPoductSixth)).click();
+			break;
+			
+		default:
+			System.out.println("You should inform: 1, 2, 3, 4, 5 or 6!");
+		}
 		
 	}
 	
