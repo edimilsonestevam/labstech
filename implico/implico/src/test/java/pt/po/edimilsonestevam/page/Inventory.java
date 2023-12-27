106627295b3df3bcd211edb9764d3a1220202e1c
Edimilson Estevam <edimilsonestevam@gmail.com>
2023-12-27 00:34:48 +0000

    Technical Challenges
    I added Inventory 's removeProductCart method in the project.

--- implico/implico/src/test/java/pt/po/edimilsonestevam/page/Inventory.java
+++ implico/implico/src/test/java/pt/po/edimilsonestevam/page/Inventory.java
@@ -55,9 +55,46 @@ public class Inventory extends Base {
 		
 	}
 	
-	public void removeProductCart () {
+	public void removeProductCart (int informProductNumber) {
 		
 		System.out.println("• Removing Product Cart...");
+		
+		String idRemoveProductFirst = "remove-sauce-labs-backpack";
+		String idRemoveProductSecond = "remove-sauce-labs-bike-light";
+		String idRemoveProductThird = "remove-sauce-labs-bolt-t-shirt";
+		String idRemoveProductFourth = "remove-sauce-labs-fleece-jacket";
+		String idRemoveProductFifth = "remove-sauce-labs-onesie";
+		String idRemovePoductSixth = "remove-test.allthethings()-t-shirt-(red)";
+		
+		
+		switch (informProductNumber) {
+		case 1:
+			browser.findElement(By.id(idRemoveProductFirst)).click();	
+			break;
+		
+		case 2:
+			browser.findElement(By.id(idRemoveProductSecond)).click();
+			break;
+		
+		case 3:
+			browser.findElement(By.id(idRemoveProductThird)).click();
+			break;
+		
+		case 4:
+			browser.findElement(By.id(idRemoveProductFourth)).click();
+			break;
+		
+		case 5:
+			browser.findElement(By.id(idRemoveProductFifth)).click();
+			break;
+			
+		case 6:
+			browser.findElement(By.id(idRemovePoductSixth)).click();
+			break;
+			
+		default:
+			System.out.println("You should inform: 1, 2, 3, 4, 5 or 6!");
+		}
 		
 	}
 	
