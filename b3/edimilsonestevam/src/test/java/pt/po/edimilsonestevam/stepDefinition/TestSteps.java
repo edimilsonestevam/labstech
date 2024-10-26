7f3bad10838ef40099f00287b56c4cb992372bbe
edimilsonestevam <edimilsonestevam@gmail.com>
2024-10-26 19:58:39 +0100

    Technical Challenges
    I am adding the TestSteps class to the project.

--- b3/edimilsonestevam/src/test/java/pt/po/edimilsonestevam/stepDefinition/TestSteps.java
+++ b3/edimilsonestevam/src/test/java/pt/po/edimilsonestevam/stepDefinition/TestSteps.java
@@ -5,10 +5,28 @@ import org.openqa.selenium.WebDriver;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import pt.po.edimilsonestevam.setup.Configuration;
 
 public class TestSteps {
 
 	protected static WebDriver browser;
 	
+	@Given("^I want to register my car in the Enter Vehicle Data$")
+	public void i_want_to_register_my_car_in_the_Enter_Vehicle_Data() throws Throwable {
+
+	}
+
+	@When("^I fill all fields \"([^\"]*)\", (\\d+), (\\d+), (\\d+), (\\d+), \"([^\"]*)\", \"([^\"]*)\", (\\d+)$")
+	public void i_fill_all_fields(String arg1, int arg2, int arg3, int arg4, int arg5, String arg6, String arg7, int arg8) throws Throwable {
+
+	}
+
+	@When("^I confirm action$")
+	public void i_confirm_action() throws Throwable {
+
+	}
+
+	@Then("^the system will show the success message \"([^\"]*)\"$")
+	public void the_system_will_show_the_success_message(String arg1) throws Throwable {
+
+	}	
 }	
