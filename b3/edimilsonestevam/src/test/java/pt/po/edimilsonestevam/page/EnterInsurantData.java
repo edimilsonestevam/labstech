63d67967040fc31e2afdd39633d6d47c1834ac5b
edimilsonestevam <edimilsonestevam@gmail.com>
2024-10-27 22:24:56 +0000

    Technical Challenges
    I am adding EnterInsurantData feature and class to the project.

--- b3/edimilsonestevam/src/test/java/pt/po/edimilsonestevam/page/EnterInsurantData.java
+++ b3/edimilsonestevam/src/test/java/pt/po/edimilsonestevam/page/EnterInsurantData.java
@@ -11,4 +11,38 @@ public class EnterInsurantData extends Base{
 
 	}
 
+	public void fillEnterInsurantData() {
+		
+		System.out.println("Enter Insurant Data");
+		
+		String idFirstName = "firstname";
+		String idLastName = "lastname";
+		String idDateBirth = "birthdate";
+		String idGenderMaale = "gendermale";
+		String idGenderFemale = "genderfemale";
+		String idStreetAddress = "streetaddress";
+		String idCountry = "country";
+		
+		String xpathCountryFirst = "//option[@value='Afghanistan']";
+		String xpathCountrySecond = "//option[@value='Albania']";
+		String xpathCountryThird = "//option[@value='Algeria']";
+		String xpathCountryFourth = "//option[@value='American Samoa']";
+		String xpathCountryFifth = "//option[@value='Andorra']";
+		String xpathCountrySixth = "//option[@value='Angola']";
+		String xpathCountrySeventh = "//option[@value='Anguilla']";
+		String xpathCountryEighth = "//option[@value='Antarctica']";
+		String xpathCountryNineth = "//option[@value='Antigua and Barbuda']";
+		String xpathCountryTenth = "//option[@value='Argentina']";
+		String xpathCountryEleventh = "//option[@value='Armenia']";
+		String xpathCountryTwelfth = "//option[@value='Aruba']";
+		String xpathCountryThirteenth = "//option[@value='Australia']";
+		String xpathCountryFourteenth = "//option[@value='Austria']";
+		String xpathCountryFifteenth = "//option[@value='Azerbaijan']";
+		String xpathCountrySixteenth = "//option[@value='Bahamas']";
+		String xpathCountrySeventeenth = "//option[@value='Bahrain']";
+		String xpathCountryEighteenth = "//option[@value='Bangladesh']";
+		String xpathCountryNineteenth = "//option[@value='Barbados']";
+		String xpathCountryTwentieth = "//option[@value='Belarus']";
+		
+	}
 }
