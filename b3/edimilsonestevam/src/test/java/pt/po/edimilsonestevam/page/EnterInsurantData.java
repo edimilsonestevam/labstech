a7a53f91f3bfb1b9a9dbcf584bcfa58f54fc305f
edimilsonestevam <edimilsonestevam@gmail.com>
2024-10-27 23:56:49 +0000

    Technical Challenges
    I am updating EnterInsurantData class in the project.

--- b3/edimilsonestevam/src/test/java/pt/po/edimilsonestevam/page/EnterInsurantData.java
+++ b3/edimilsonestevam/src/test/java/pt/po/edimilsonestevam/page/EnterInsurantData.java
@@ -15,7 +15,7 @@ public class EnterInsurantData extends Base{
 
 	}
 
-	public void fillEnterInsurantData(String informFirstName, String informLastName, String informDateBirth, String informGender, String informStreetAddress) {
+	public void fillEnterInsurantData(String informFirstName, String informLastName, String informDateBirth, String informGender, String informStreetAddress, String informCountry) {
 		
 		System.out.println("Enter Insurant Data");
 		
@@ -30,28 +30,70 @@ public class EnterInsurantData extends Base{
 		String idGenderFemale = "genderfemale";
 		
 		String idStreetAddress = "streetaddress";
+		
 		String idCountry = "country";
 		
+		String countryFirst = "Angola";
 		String xpathCountryFirst = "//option[@value='Angola']";
+		
+		String countrySecond = "Argentina";
 		String xpathCountrySecond = "//option[@value='Argentina']";
+		
+		String countryThird = "Australia";
 		String xpathCountryThird = "//option[@value='Australia']";
+		
+		String countryFourth = "Austria";
 		String xpathCountryFourth = "//option[@value='Austria']";
+		
+		String countryFifth = "Belgium";
 		String xpathCountryFifth = "//option[@value='Belgium']";
+		
+		String countrySixth = "Brazil";
 		String xpathCountrySixth = "//option[@value='Brazil']";
+		
+		String countrySeventh = "Canada";
 		String xpathCountrySeventh = "//option[@value='Canada']";
+		
+		String countryEighth = "Chile";
 		String xpathCountryEighth = "//option[@value='Chile']";
+		
+		String countryNineth = "China";
 		String xpathCountryNineth = "//option[@value='China']";
+		
+		String countryTenth = "Colombia";
 		String xpathCountryTenth = "//option[@value='Colombia']";
+		
+		String countryEleventh = "Costa Rica";
 		String xpathCountryEleventh = "//option[@value='Costa Rica']";
+		
+		String countryTwelfth = "Croatia";
 		String xpathCountryTwelfth = "//option[@value='Croatia']";
+		
+		String countryThirteenth = "Czech Republic";
 		String xpathCountryThirteenth = "//option[@value='Czech Republic']";
+		
+		String countryFourteenth = "Denmark";
 		String xpathCountryFourteenth = "//option[@value='Denmark']";
+		
+		String countryFifteenth = "Ecuador";
 		String xpathCountryFifteenth = "//option[@value='Ecuador']";
+		
+		String countrySixteenth = "Estonia";
 		String xpathCountrySixteenth = "//option[@value='Estonia']";
+		
+		String countrySeventeenth = "Finland";
 		String xpathCountrySeventeenth = "//option[@value='Finland']";
+		
+		String countryEighteenth = "France";
 		String xpathCountryEighteenth = "//option[@value='France']";
+		
+		String countryNineteenth = "Georgia";
 		String xpathCountryNineteenth = "//option[@value='Georgia']";
+		
+		String countryTwentieth = "Germany";
 		String xpathCountryTwentieth = "//option[@value='Germany']";
+		
+		String countryTwentyFirst = "Hong Kong";
 		String xpathCountryTwentyFirst = "//option[@value='Hong Kong']";
 		
 		String idZipCode = "zipcode";
@@ -117,6 +159,86 @@ public class EnterInsurantData extends Base{
 		
 		
 		System.out.println("• Filling Country Field...");
+		
+		if (informCountry.equalsIgnoreCase(carOptionFirst)) {
+			browser.findElement(By.id(idCountry)).click();
+			element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpathCarOptionFirst)));
+			element.click();
+		}
+		else if (informCountry.equalsIgnoreCase(carOptionSecond)) {
+			browser.findElement(By.id(idCountry)).click();
+			element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpathCarOptionSecond)));
+			element.click();
+		}
+		else if (informCountry.equalsIgnoreCase(carOptionThird)) {
+			browser.findElement(By.id(idCountry)).click();
+			element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpathCarOptionThird)));
+			element.click();
+		}
+		else if (informCountry.equalsIgnoreCase(carOptionFourth)) {
+			browser.findElement(By.id(idCountry)).click();
+			element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpathCarOptionFourth)));
+			element.click();
+		}
+		else if (informCountry.equalsIgnoreCase(carOptionFifth)) {
+			browser.findElement(By.id(idCountry)).click();
+			element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpathCarOptionFifth)));
+			element.click();
+		}
+		else if (informCountry.equalsIgnoreCase(carOptionSixth)) {
+			browser.findElement(By.id(idCountry)).click();
+			element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpathCarOptionSixth)));
+			element.click();
+		}
+		else if (informCountry.equalsIgnoreCase(carOptionSeventh)) {
+			browser.findElement(By.id(idCountry)).click();
+			element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpathCarOptionSeventh)));
+			element.click();
+		}
+		else if (informCountry.equalsIgnoreCase(carOptionEighth)) {
+			browser.findElement(By.id(idCountry)).click();
+			element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpathCarOptionEighth)));
+			element.click();
+		}
+		else if (informCountry.equalsIgnoreCase(carOptionNinth)) {
+			browser.findElement(By.id(idCountry)).click();
+			element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpathCarOptionNinth)));
+			element.click();
+		}
+		else if (informCountry.equalsIgnoreCase(carOptionTenth)) {
+			browser.findElement(By.id(idCountry)).click();
+			element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpathCarOptionTenth)));
+			element.click();
+		}
+		else if (informCountry.equalsIgnoreCase(carOptionEleventh)) {
+			browser.findElement(By.id(idCountry)).click();
+			element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpathCarOptionEleventh)));
+			element.click();
+		}
+		else if (informCountry.equalsIgnoreCase(carOptionTwelfth)) {
+			browser.findElement(By.id(idCountry)).click();
+			element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpathCarOptionTwelfth)));
+			element.click();
+		}
+		else if (informCountry.equalsIgnoreCase(carOptionThirteenth)) {
+			browser.findElement(By.id(idCountry)).click();
+			element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpathCarOptionThirteenth)));
+			element.click();
+		}
+		else if (informCountry.equalsIgnoreCase(carOptionFourteenth)) {
+			browser.findElement(By.id(idCountry)).click();
+			element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpathCarOptionFourteenth)));
+			element.click();
+		}
+		else if (informCountry.equalsIgnoreCase(carOptionFifteenth)) {
+			browser.findElement(By.id(idCountry)).click();
+			element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpathCarOptionFifteenth)));
+			element.click();
+		}
+		else {
+			System.out.println("You should inform: Angola, Argentina, Australia, Austria, Belgium, Brazil, Canada, Chile, China, Colombia, Costa Rica, Croatia, Czech Republic, Denmark, Ecuador, Estonia, Finland, France, Georgia, Germany or Hong Kong!");
+		}
+		
 		
 		System.out.println("• Filling Zip Code Field...");
 		
