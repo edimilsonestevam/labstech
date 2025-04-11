24af23700f638560715f44f9a2a3251e9aa1fe3c
edimilsonestevam <edimilsonestevam@gmail.com>
2025-04-11 04:12:13 -0300

    Technical Challenges
    I updated POM file in the ilegra project.

--- ilegra/ilegra/src/test/java/br/sp/edimilsonestevam/cucumberTest/TestSuite.java
+++ ilegra/ilegra/src/test/java/br/sp/edimilsonestevam/cucumberTest/TestSuite.java
@@ -1,13 +1,13 @@
 package br.sp.edimilsonestevam.cucumberTest;
 
 import org.junit.runner.RunWith;
-import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {"src/test/java/br/sp/edimilsonestevam/feature"}
                 , glue = { "br/sp/edimilsonestevam/stepDefinition" }
-				, plugin = {"html:target/cucumber-reports"}
+				, plugin = {"pretty", "html:target/cucumber-report.html"}
 				, dryRun = false
 				, monochrome = true)
 
