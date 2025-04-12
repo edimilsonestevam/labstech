42f988c7c8b0802e5f4cc77977a8865378b3cb22
edimilsonestevam <edimilsonestevam@gmail.com>
2025-04-12 20:35:14 -0300

    Technical Challenges
    I updated SearchNewsArticle (Java) in the ilegra project.

--- ilegra/ilegra/src/test/java/br/sp/edimilsonestevam/page/SearchNewsArticle.java
+++ ilegra/ilegra/src/test/java/br/sp/edimilsonestevam/page/SearchNewsArticle.java
@@ -1,13 +1,13 @@
 package br.sp.edimilsonestevam.page;
 
 import br.sp.edimilsonestevam.setup.Base;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.awt.*;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 import java.time.Duration;
 
 public class SearchNewsArticle extends Base {
@@ -15,18 +15,36 @@ public class SearchNewsArticle extends Base {
         super(browser);
     }
 
-    public void findNewsArticle(String searchNewsArticle){
+    public void findNewsArticle(String searchNewsArticle) throws AWTException {
 
         System.out.println("• Searching News Article...");
 
-        String xpathSearchNewsArticle = "//span[contains(@class,'ast-icon icon-search icon-search')]//span[contains(@class,'ahfb-svg-iconset ast-inline-flex svg-baseline')]//*//*";
-        WebElement element = null;
+        String locator = "//button[contains(@class, 'icon-search')]";
 
-        WebDriverWait wait = new WebDriverWait(browser, Duration.ofSeconds(5));
-        element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpathSearchNewsArticle)));
-        element.click();
-        element.sendKeys(searchNewsArticle);
-        element.sendKeys(Keys.ENTER);
+        WebDriverWait wait = new WebDriverWait(browser, Duration.ofSeconds(10));
+        
+        try {
 
+            WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(locator)));
+
+            wait.until(ExpectedConditions.visibilityOf(element));
+
+            ((JavascriptExecutor) browser).executeScript("arguments[0].scrollIntoView({block: 'center'});", element);
+
+            try {
+
+                wait.until(ExpectedConditions.elementToBeClickable(element)).click();
+
+            } catch (Exception e) {
+
+                ((JavascriptExecutor) browser).executeScript("arguments[0].click();", element);
+
+            }
+
+        } catch (TimeoutException e) {
+
+            throw new RuntimeException("Element can not be clicked: " + locator.toString(), e);
+
+        }
     }
 }
