5ba03706d063febec78b40a843187a0d92fcf48c
edimilsonestevam <edimilsonestevam@gmail.com>
2026-02-05 18:17:44 +0000

    Technical Challenges

--- implico/implico/src/test/java/pt/po/edimilsonestevam/stepDefinition/TestSteps.java
+++ implico/implico/src/test/java/pt/po/edimilsonestevam/stepDefinition/TestSteps.java
@@ -2,9 +2,9 @@ package pt.po.edimilsonestevam.stepDefinition;
 
 import org.openqa.selenium.WebDriver;
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import pt.po.edimilsonestevam.page.Cart;
 import pt.po.edimilsonestevam.page.Inventory;
 import pt.po.edimilsonestevam.page.Login;
